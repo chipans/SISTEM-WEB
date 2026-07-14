8b23857c16d6b74d48ad16b6fc2029a80ed9588a
elprogramador789 <https://github.com/elprogramador789/lab7-html.git>
2026-07-14 11:37:24 -0400

    fix(auth/login): Reduce the size of the background logo on the login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,13 +11,7 @@ export const routes: Routes = [
     path: 'login', 
     loadComponent: () => 
       import('./features/auth/login/login.component').then(m => m.default),
-  },
-  { 
-    path: 'register', 
-    loadComponent: () => 
-      import('./features/auth/register/register.component').then(m => m.default),
-  },
-  { 
+  },{ 
     path: 'dashboard', 
     loadChildren: () => Promise.resolve(dashboardRoutes),
   },
